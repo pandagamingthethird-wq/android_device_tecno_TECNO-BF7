ab131a7d7c3665ad72fe1bae656fa7cb32a0d4ac
pandagamingthethird-wq <pandagamingthethird@gmail.com>
2026-09-09 19:33:10 +0800

    Update twrp_BF7.mk

--- twrp_BF7.mk
+++ twrp_BF7.mk
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -24,3 +16,7 @@ PRODUCT_MANUFACTURER := TECNO
 
 PRODUCT_GMS_CLIENTID_BASE := android-tecno
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_bf7_h6127-user 12 SP1A.210812.016 651674 release-keys"
+
+BUILD_FINGERPRINT := TECNO/BF7/TECNO-BF7:12/SP1A.210812.016/240703V1812:user/release-keys
